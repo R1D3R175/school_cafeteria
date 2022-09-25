05312e58159798eeca1d5df62da907addb697da2
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-25 21:04:33 +0200

    Refactored index endpoint with login_required

--- www/index/routes.py
+++ www/index/routes.py
@@ -1,36 +1,13 @@
-from flask import Blueprint, render_template, request
-from flask_login import login_user
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user, login_required
 
-from www.models import User
+index_bp = Blueprint('index', __name__, url_prefix='/')
 
-index = Blueprint('index', __name__)
-
-def authenticate(class_name: str, password: str, remember: bool) -> bool:
-    class_query = User.query.filter(User.username.upper() == class_name.upper()).first()
-    
-    if not class_query:
-        return False
-    
-    if check_password_hash(class_query.password, password):
-        login_user(class_query, remember=remember)
-        return True
-    
-    return False
-        
-
-@index.route('/')
-def main():
-    return render_template('index.html')
-
-@index.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
+@index_bp.route('/', methods=["GET", "POST"])
+@index_bp.route('/order', methods=["GET", "POST"])
+@login_required
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
     else:
-        class_input = request.form['class-input']
-        password_input = request.form['password-input']
-        remember_checkbox = request.form['remember-checkbox'] == "remember"
-        
-        if authenticate(class_input, password_input, remember_checkbox):
-            return render_template('')
+        pass
