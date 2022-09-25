0d5ee6344fbd793bf0abf3fcf3baaae61980774f
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-26 00:24:57 +0200

    WIP: Dynamic form

--- www/static/order_form.js
+++ www/static/order_form.js
@@ -1,21 +1,54 @@
-// Fetches available pieces and initializes form
-// when the document is ready
-function create_form() {
-    var $form_container = $("#form-container")
-    var $dropdown = $("<div>", { "class": "container" })
+function get_dropdown_list_from(endpoint) {
+    var $dropdown_list = $("<ul>", { "class": "dropdown-menu "})
+    $.ajax({
+        method: "GET",
+        url: endpoint,
+        dataType: "json",
+        success: function(results) {
+            var result_name = Object.keys(results)[0]
+            for (const result of results[result_name]) {
+                $("<li>").append($("<a>", {
+                    text: result["name"],
+                    "class": `dropdown-item ${result_name}`,
+                    "href": "#",
+                    "api_id": result['id']
+                })).appendTo($dropdown_list)
+            }
+        }
+    })
+
+    return $dropdown_list
+}
+
+function create_dropdown_from(endpoint, button_text) {
+    var $dropdown = $("<div>", { "class": "dropdown" })
     var $dropdown_button = $("<button>", {
-        text: "Add a piece",
+        text: button_text,
         "class": "btn btn-secondary dropdown-toggle",
         "type": "button",
         "data-bs-toggle": "dropdown",
-        "aria-expanded": "false",
+        "aria-expanded": "false"
     })
+    
+    var $dropdown_list = get_dropdown_list_from(endpoint)
+    $dropdown.append($dropdown_button, $dropdown_list)
 
-    $dropdown_button.appendTo($dropdown)
+    return $dropdown
+}
 
-    $form_container.append($dropdown)
+// Fetches available pieces and initializes form when the document is ready
+function create_form() {
+    var $form_container = $("#form-container")
+
+    var $food_dropdown = create_dropdown_from("/api/get_foods", "Add food")
+    var $drink_dropdown = create_dropdown_from("/api/get_drinks", "Add drink")
+    $form_container.append($food_dropdown, $drink_dropdown)
 }
 
 $(function () {
     create_form()
+})
+
+$(document).on("click", ".dropdown-item", function(element) {
+    console.log(element.target)
 })
