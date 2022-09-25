25bb8fbafb6793f4424fcd43e16d5c5bc5129bf9
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-25 21:03:55 +0200

    Registered blueprints and added login_view

--- www/__init__.py
+++ www/__init__.py
@@ -4,17 +4,20 @@ from flask_login import LoginManager
 from www.models import db
 from www.models import User
 
-from www.index.routes import index
+from www.index.routes import index_bp
+from www.login.routes import login_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
 
-    app.register_blueprint(index, url_prefix='/')
+    app.register_blueprint(index_bp, url_prefix='/')
+    app.register_blueprint(login_bp, url_prefix='/login')
 
     db.init_app(app)
 
     login_manager = LoginManager()
+    login_manager.login_view = "login.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
