2e4be24d45504405c47ccc8aba49d7a3d97028dc
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-26 20:31:54 +0200

    Added route to get food by ID

--- www/api/routes.py
+++ www/api/routes.py
@@ -8,6 +8,14 @@ api_bp = Blueprint('api', __name__, url_prefix='/api')
 def get_foods():
     return jsonify(food=Food.query.all())
 
+@api_bp.route('/get_foods/<int:id>')
+def get_food_by_id(id):
+    return jsonify(food=Food.query.filter(Food.id == id).first())
+
 @api_bp.route('/get_drinks')
 def get_drinks():
     return jsonify(drink=Drink.query.all())
+
+@api_bp.route('/get_drinks/<int:id>')
+def get_drink_by_id(id):
+    return jsonify(drink=Drink.query.filter(Drink.id == id).first())
