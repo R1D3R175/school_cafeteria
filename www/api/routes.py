7b1ef59b6ae68bd232d7cbfefed841f002a965c8
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-26 21:33:18 +0200

    Changed api endpoints

--- www/api/routes.py
+++ www/api/routes.py
@@ -1,29 +1,29 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect, url_for
 
 from www.models import Food, Drink
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
-@api_bp.route('/get_food')
+@api_bp.route('/get/food/')
 def get_foods():
     return jsonify(food=Food.query.all())
 
-@api_bp.route('/get_food/<int:id>')
+@api_bp.route('/get/food/<int:id>')
 def get_food_by_id(id):
     return jsonify(food=Food.query.filter(Food.id == id).first())
 
-@api_bp.route('/get_food/exclude/<int_list:values>')
+@api_bp.route('/get/food/<int_list:values>')
 def exclude_foods(values):
     return jsonify(food=Food.query.filter(Food.id.not_in(values)).all())
 
-@api_bp.route('/get_drink')
+@api_bp.route('/get/drink/')
 def get_drinks():
     return jsonify(drink=Drink.query.all())
 
-@api_bp.route('/get_drink/<int:id>')
+@api_bp.route('/get/drink/<int:id>')
 def get_drink_by_id(id):
     return jsonify(drink=Drink.query.filter(Drink.id == id).first())
 
-@api_bp.route('/get_drink/exclude/<int_list:values>')
+@api_bp.route('/get/drink/<int_list:values>')
 def exclude_drinks(values):
     return jsonify(drink=Drink.query.filter(Drink.id.not_in(values)).all())
