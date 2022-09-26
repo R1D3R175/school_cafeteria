ccdcd935c9d4912f31ef0cd31d9302844accc38f
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-26 20:32:37 +0200

    (WIP) Added selector for number of food

--- www/static/order_form.js
+++ www/static/order_form.js
@@ -1,3 +1,4 @@
+
 function get_dropdown_list_from(endpoint) {
     var $dropdown_list = $("<ul>", { "class": "dropdown-menu "})
     $.ajax({
@@ -11,6 +12,8 @@ function get_dropdown_list_from(endpoint) {
                     text: result["name"],
                     "class": `dropdown-item ${result_name}`,
                     "href": "#",
+                    "api_source": endpoint,
+                    "api_type": result_name,
                     "api_id": result['id']
                 })).appendTo($dropdown_list)
             }
@@ -20,8 +23,11 @@ function get_dropdown_list_from(endpoint) {
     return $dropdown_list
 }
 
-function create_dropdown_from(endpoint, button_text) {
-    var $dropdown = $("<div>", { "class": "dropdown" })
+function create_dropdown_from(endpoint, button_text) { 
+    var $dropdown_list = get_dropdown_list_from(endpoint)
+    var $dropdown = $("<div>", { 
+        "class": `dropdown`
+    })   
     var $dropdown_button = $("<button>", {
         text: button_text,
         "class": "btn btn-secondary dropdown-toggle",
@@ -29,26 +35,94 @@ function create_dropdown_from(endpoint, button_text) {
         "data-bs-toggle": "dropdown",
         "aria-expanded": "false"
     })
-    
-    var $dropdown_list = get_dropdown_list_from(endpoint)
+
     $dropdown.append($dropdown_button, $dropdown_list)
 
     return $dropdown
 }
 
-// Fetches available pieces and initializes form when the document is ready
 function create_form() {
-    var $form_container = $("#form-container")
+    var $form_container = $("#order-container")
 
     var $food_dropdown = create_dropdown_from("/api/get_foods", "Add food")
     var $drink_dropdown = create_dropdown_from("/api/get_drinks", "Add drink")
     $form_container.append($food_dropdown, $drink_dropdown)
 }
 
-$(function () {
-    create_form()
+function get_by_id(endpoint, id) {
+    endpoint += `/${id}`
+
+    return Promise.resolve($.ajax({
+        method: "GET",
+        url: endpoint,
+        dataType: "json",
+    }))
+}
+
+$(document).on("click", ".dropdown-item", async function() {
+    var data = (await get_by_id($(this).attr("api_source"), $(this).attr("api_id")))[$(this).attr("api_type")]
+
+    var name = data["name"]
+    var price = data["price"].toFixed(2)
+    
+    var $card = $("<div>", {
+        "class": "card"
+    })
+
+    $("<div>", {
+        text: `${name} - ${price}\u20AC`,
+        "class": "card-header"
+    }).appendTo(
+        $card
+    )
+
+    var $card_ul = $("<ul>", {
+        "class": "list-group list-group-flush"
+    })
+
+    $("<input>", {
+        "type": "range",
+        "class": `form-range order-range`,
+        "min": "1",
+        "max": "10",
+        "value": "1",
+        "price": price
+    }).appendTo(
+        $("<li>", {
+            "class": "list-group-item"
+        })
+    ).appendTo($card_ul)
+
+    $("<li>", {
+        text: `x1 ${price}\u20AC`,
+        "class": "list-group-item order-info"
+    }).appendTo($card_ul)
+
+    $card_ul.appendTo($card)
+    
+    $(".dropdown").remove()
+
+    $card.appendTo($("#order-container"))
+
+    $("<button>", {
+        text: "Order",
+        "type": "submit",
+        "class": "btn btn-primary"
+    }).appendTo($("form"))
 })
 
-$(document).on("click", ".dropdown-item", function(element) {
-    console.log(element.target)
+$(document).on("input", ".order-range", function() {
+    var $parent = $(this).parent()
+    var $order_range = $parent.children("input.order-range")
+    var $order_info = $parent.children("li.order-info")
+    
+    var amount = parseInt($order_range.val())
+    var total_price = (amount * parseFloat($order_range.attr("price")))
+
+    var final_string = `x${amount} ${total_price.toFixed(2)}\u20AC`
+    $order_info.text(final_string)
+})
+
+$(function () {
+    create_form()
 })
