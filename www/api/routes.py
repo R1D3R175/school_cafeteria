d4bb34a382a8a673d000d0b5568abde2367fa05d
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-26 21:02:41 +0200

    Added "Exclude" API

--- www/api/routes.py
+++ www/api/routes.py
@@ -4,18 +4,26 @@ from www.models import Food, Drink
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
-@api_bp.route('/get_foods')
+@api_bp.route('/get_food')
 def get_foods():
     return jsonify(food=Food.query.all())
 
-@api_bp.route('/get_foods/<int:id>')
+@api_bp.route('/get_food/<int:id>')
 def get_food_by_id(id):
     return jsonify(food=Food.query.filter(Food.id == id).first())
 
-@api_bp.route('/get_drinks')
+@api_bp.route('/get_food/exclude/<int_list:values>')
+def exclude_foods(values):
+    return jsonify(food=Food.query.filter(Food.id.not_in(values)).all())
+
+@api_bp.route('/get_drink')
 def get_drinks():
     return jsonify(drink=Drink.query.all())
 
-@api_bp.route('/get_drinks/<int:id>')
+@api_bp.route('/get_drink/<int:id>')
 def get_drink_by_id(id):
     return jsonify(drink=Drink.query.filter(Drink.id == id).first())
+
+@api_bp.route('/get_drink/exclude/<int_list:values>')
+def exclude_drinks(values):
+    return jsonify(drink=Drink.query.filter(Drink.id.not_in(values)).all())
