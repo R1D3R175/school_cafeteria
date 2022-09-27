dededa437313c3799c4ffa45437142987ca875ff
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-27 19:47:10 +0200

    Removed useless things

--- www/index/routes.py
+++ www/index/routes.py
@@ -3,11 +3,7 @@ from flask_login import current_user, login_required
 
 index_bp = Blueprint('index', __name__, url_prefix='/')
 
-@index_bp.route('/', methods=["GET", "POST"])
-@index_bp.route('/order', methods=["GET", "POST"])
+@index_bp.route('/')
 @login_required
 def index():
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        pass
+    return render_template("index.html")
