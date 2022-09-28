4036604807442f1e70611bb02f7d98024978d5f1
R1D3R175 <r1.d3.r175@gmail.com>
2022-09-28 21:46:48 +0200

    Implement handle_order base logic

--- www/api/routes.py
+++ www/api/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, Response
-from flask_login import login_required
+from flask_login import login_required, current_user
 
-from www.models import Food, Drink
+from www.models import Food, Drink, Order, FoodInOrder, db
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -32,5 +32,29 @@ def exclude_drinks(values):
 @login_required
 @api_bp.route('/handle_order', methods=["POST"])
 def handle_order():
-    print(request.get_json())
-    return Response(status=400)
+    try:
+        # TODO: Verify if user has already order, if so
+        #       check if it's expired, if not 400 status
+        json_request = request.get_json()
+
+        order = Order(user=current_user)
+        db.session.add(order)
+
+        for food_json in json_request["food"]:
+            food = Food.query.filter(Food.id == food_json["id"]).first()
+            food_in_order = FoodInOrder(order=order, food=food, amount=food_json["amount"])
+
+            db.session.add(food_in_order)
+
+        for drink_json in json_request["drink"]:
+            drink = Drink.query.filter(Drink.id == drink_json["id"]).first()
+            drink_in_order = FoodInOrder(order=order, drink=drink, amount=drink_json["amount"])
+
+            db.session.add(drink_in_order)
+    except:
+        return Response(status=400)
+            
+    db.session.commit()
+    return Response(status=200)
+
+# TODO: Implement cafeteria worker
